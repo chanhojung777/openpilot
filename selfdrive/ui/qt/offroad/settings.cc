a7976e4b92d739185c2425878fdd0d1d3f39c9a7
ajouatom <ajouatom@gmail.com>
2024-12-13 18:41:43 +0900

    fix..

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -246,7 +246,7 @@ DevicePanel::DevicePanel(SettingsWindow *parent) : ListWidget(parent) {
     if (ConfirmationDialog::confirm(tr("Are you sure you want to reset calibration?"), tr("Reset"), this)) {
       params.remove("CalibrationParams");
       params.remove("LiveTorqueParameters");
-      emit closeSettings();
+      emit parent->closeSettings();
       QTimer::singleShot(1000, []() {
         Params().putBool("SoftRestartTriggered", true);
       });
