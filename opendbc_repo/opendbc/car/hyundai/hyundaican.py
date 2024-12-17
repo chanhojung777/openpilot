39f192dc507c59c03c3fafcac731604f0536e4cb
ajouatom <ajouatom@gmail.com>
2024-12-17 09:02:02 +0900

    fix.. hyundaican

--- opendbc_repo/opendbc/car/hyundai/hyundaican.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaican.py
@@ -308,7 +308,7 @@ def create_acc_commands(packer, enabled, accel, jerk, idx, hud_control, set_spee
 
   return commands
 
-def create_acc_opt(packer, CP):
+def create_acc_opt(packer, CP, CP):
   commands = []
 
   scc13_values = {
